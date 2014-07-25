97a9665e8fe5e37e346bc8f9caf4ac22aca418b3
karthikds <karthikds_619@yahoo.com>
2014-07-25 18:40:38 +0530

    added a simple rspec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Book do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has an author" do
+  	book = Book.new(author: "tester")
+  	expect(book.author).to be_present
+  end
+
+  it "must have an author" do
+  	book = Book.new
+  	expect(book.author).to be_present
+  end
+
+
 end
